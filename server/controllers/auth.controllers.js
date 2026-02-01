760d0d719a889b6cf0a9c6b6cf7137771f53243a
Reno-mathews <renomathews10pro@gmail.com>
2026-02-01 21:30:32 +0530

    adding authcontroller.js

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,3 +1,17 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const USer = require("")9
+const { pool } = require("../db");
+
+exports.signup = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: "Email and password required" });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await
+    }
+}
