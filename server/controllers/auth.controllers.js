1f82a82110ab9ee0b3de49a97a21020ba9196893
Reno-mathews <renomathews10pro@gmail.com>
2026-02-01 21:34:35 +0530

    adding authcontroller.js

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -12,6 +12,13 @@ exports.signup = async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const result = await
+        const result = await pool.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id",
+            [email, hashedPassword]
+        );
+
+        const token = jwt.sign(
+            
+        )
     }
 }
