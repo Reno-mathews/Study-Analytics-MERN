78e1dde417d8a162f62893800297d9e7f423d16e
Reno-mathews <renomathews10pro@gmail.com>
2026-02-01 18:48:51 +0530

    adding postgres

--- server/index.js
+++ server/index.js
@@ -22,6 +22,30 @@ app.get("/users", async (req, res) => {
     }
 });
 
+app.post("/signup", async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+        return res.status(400).json({ error: "Email and password required" });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const result = await pool.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email",
+            [email, hashedPassword]
+        );
+
+        const token = JsonWebTokenError.sign(
+            { userId: result.rows[0].id },
+            process.env.JWT_SECRET,
+            { expiresIn: "1d"}
+        );
+        
+    }
+})
+
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
 });
