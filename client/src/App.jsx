847df1e4edc5be1942dc412c00c2a555244a7bc9
Reno-mathews <renomathews10pro@gmail.com>
2026-02-01 11:04:32 +0530

    installed cors express bcrypt mongoose and jsonwebtoken

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import LogoutButton from './components/LogoutButton';
+import { login, signup} from './components/AuthState';
+import Header from "./components/Header";
+import Board from './components/Board';
 function App() {
+  const [user, setUser] = useState(null);
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [email, setEmail] = useState("");
+  const [oassword, setPassword] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser({ token });
+    }
+  }, []);
+
+  const handleLogin = async (email, password) => {
+    const user = await login(email, password);
+    setUser(user);
+  };
+
+  const handleSignup = async
+
 }
 
 export default App
