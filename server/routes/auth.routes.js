89fac4e17803729ea1c3c87bd4d41d8456686b9b
Reno-mathews <renomathews10pro@gmail.com>
2026-02-02 13:12:09 +0530

    adding authroute

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,3 +2,8 @@ const express = require("express");
 const { signup, login } = require("./controllers/auth.controller");
 
 const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+
+module.exports = router;
