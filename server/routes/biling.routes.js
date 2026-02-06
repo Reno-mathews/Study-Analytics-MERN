0c66b94afe082e94b71479d556910d2cad9d62aa
Reno-mathews <renomathews10pro@gmail.com>
2026-02-06 19:24:54 +0530

    Adding stripe payment

---
+++ server/routes/biling.routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const Stripe = require("stripe");
+const authMiddleware = require("../middleware/auth.middleware");
+
+const router = express.router();
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+router.post(
+    "/create-checkout-session",
+    authMiddleware,
+    async (req, res) => {
+        try {
+            const session = await stripe.checkout.sessions.create({
+                mode: "subscription",
+                payment_method_types: ["card"],
+                line_items: [
+                    {
+                        price: process.env.STRIPE_PRICE_ID,
+                        quantity: 1,
+                    },
+                ],
+                success_url: `${process.env.CLIENT_URL}/upgrade-success`,
+                cancel_url: `${process.env.CLIENT_URL}upgrade`,
+                metadata: {
+                    userId: req.userId,
+                }
+            });
+
+            res.json({ url: session.url });
+        } catch (err) {
+            console.error("Stripe error:", err);
+            res.status(500).json({ error: "Failed to create checkout session" })''
+        }
+    }
+);
+
+module.exports = router;
