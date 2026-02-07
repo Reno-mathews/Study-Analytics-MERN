ae0078bdd97724c196e63066e822c5b3b9bcfde1
Reno-mathews <renomathews10pro@gmail.com>
2026-02-07 22:20:43 +0530

    Fixing stripe.webhook.js

--- server/routes/stripe.webhook.js
+++ server/routes/stripe.webhook.js
@@ -27,6 +27,7 @@ router.post(
             const session = event.data.object;
             const userId = session.metadata.userId;
             const customerId = session.customer;
+            const subscriptionId = session.subscription;
 
             try {
                 await pool.query(
