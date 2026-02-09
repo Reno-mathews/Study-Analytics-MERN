b2639a10148622ef431053fc0302f9498aace023
Reno-mathews <renomathews10pro@gmail.com>
2026-02-09 19:43:10 +0530

    Fixing stripe.webhook.js

--- server/routes/sessions.routes.js
+++ server/routes/sessions.routes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/auth.middleware");
-
+const requirePro = require("../middleware/requirePro.middleware");
 const {
     getSessions,
     createSession,
 } = require("../controllers/sessions.controller");
 
 router.get("/", auth, getSessions);
-router.post("/", auth, createSession);
+router.post("/", auth, requirePro, createSession);
 
 module.exports = router;
 
