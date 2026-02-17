81967a749795b2543895ad65baa4f444c0371dc1
Reno-mathews <renomathews10pro@gmail.com>
2026-02-17 13:24:09 +0530

    fixing users

--- server/routes/stripe.webhook.js
+++ server/routes/stripe.webhook.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const Stripe= require("stripe");
-const pool = require("../db");
+const { pool } = require("../db");
 
 const router = express.Router();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
