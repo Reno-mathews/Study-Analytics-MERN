2ce0b423255a2a8297eafac60e701d70fb158d91
Reno-mathews <renomathews10pro@gmail.com>
2026-02-19 19:07:04 +0530

    adding more charts

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -114,7 +114,7 @@ function Board({ user }) {
         </div>
 
         <div className="bg-white border border-slate-200 rounded-lg p-5 mb-8">
-            
+            <h2 className="text-base font-semibold mb-4">Cumulative Study Time </h2>
         </div>
 
             {user?.isPro ? (
