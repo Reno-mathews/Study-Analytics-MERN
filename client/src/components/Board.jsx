e77b214da104ea370c2a9e60829a4ff79aca9158
Reno-mathews <renomathews10pro@gmail.com>
2026-02-20 12:49:43 +0530

    adding more charts

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -24,6 +24,7 @@ function Board({ user }) {
         addSession, 
         dailyChartData, 
         subjectChartData,
+        cumulativeChartData,
         totalMinutes,
         sessionCount,
         averageMinutes,
@@ -118,7 +119,7 @@ function Board({ user }) {
             <h2 className="text-base font-semibold mb-4">Cumulative Study Time </h2>
 
             <ResponsiveContainer width="100%" height={260}>
-                <AreaChart data={dailyChartData}>
+                <AreaChart data={cumulativeChartData}>
                 <CartesianGrid strokeDasharray="3 3" stroke="#e5e7eb" />
                 <XAxis dataKey="date" stroke="#64748b" />
                 <YAxis stroke="#64748b" />
