1c81e5b5e76ab4f8884d32e04b41bf9212ad989a
Reno-mathews <renomathews10pro@gmail.com>
2026-02-26 17:26:15 +0530

    Fixing alignment

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -61,11 +61,7 @@ function Board({ user }) {
                         {/* Dashboard cards */}
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
                             <DashboardCard title="Total Study Time" value={`${totalMinutes} min`} />
-
-                            <DashboardCard
-                                title="Sessions"
-                                value={sessionCount}
-                            />
+                            <DashboardCard title="Sessions" value={sessionCount} />
 
                             {user?.isPro ? (
                             <>  
